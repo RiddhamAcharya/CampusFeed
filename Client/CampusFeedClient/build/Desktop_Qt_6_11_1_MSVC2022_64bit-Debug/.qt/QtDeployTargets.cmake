7d6012279d0ea68346eda4fd928e0a005bc61451
Riddham Acharya <myselfriddham@gmail.com>
2026-05-28 21:20:14 +0545

    Fixing files

--- Client/CampusFeedClient/build/Desktop_Qt_6_11_1_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_CampusFeedClient_FILE C:/Drive D/Engineering Projects/CampusFeed/CampusFeed/Client/CampusFeedClient/build/Desktop_Qt_6_11_1_MSVC2022_64bit-Debug/CampusFeedClient.exe)
-set(__QT_DEPLOY_TARGET_CampusFeedClient_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_CampusFeedClient_RUNTIME_DLLS C:/Qt/6.11.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.11.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.11.1/msvc2022_64/bin/Qt6Cored.dll)
